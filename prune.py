16f7644735d539a90f51d0de18a9d8a9d697b397
Boyuan-Tian <546600519@qq.com>
2025-07-21 14:16:53 -0500

    Init (#1)
    * Init.
    
    * Fix enviroment.yml.
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ prune.py
@@ -0,0 +1,68 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import os
+import torch
+from random import randint
+from gaussian_renderer import render, count_render
+import sys
+from scene import Scene, GaussianModel
+from utils.general_utils import safe_state
+import uuid
+from utils.image_utils import psnr
+from argparse import ArgumentParser, Namespace
+from arguments import ModelParams, PipelineParams, OptimizationParams
+from utils.graphics_utils import getWorld2View2
+import random
+import copy
+import gc
+import numpy as np
+from collections import defaultdict
+
+def calculate_v_imp_score(gaussians, imp_list, v_pow):
+    """
+    :param gaussians: A data structure containing Gaussian components with a get_scaling method.
+    :param imp_list: The importance scores for each Gaussian component.
+    :param v_pow: The power to which the volume ratios are raised.
+    :return: A list of adjusted values (v_list) used for pruning.
+    """
+    # Calculate the volume of each Gaussian component
+    volume = torch.prod(gaussians.get_scaling, dim=1)
+    # Determine the kth_percent_largest value
+    index = int(len(volume) * 0.9)
+    sorted_volume, _ = torch.sort(volume, descending=True)
+    kth_percent_largest = sorted_volume[index]
+    # Calculate v_list
+    v_list = torch.pow(volume / kth_percent_largest, v_pow)
+    v_list = v_list * imp_list
+    return v_list
+
+def prune_list(gaussians, scene, pipe, background):
+    viewpoint_stack = scene.getTrainCameras().copy()
+    gaussian_list, imp_list = None, None
+    viewpoint_cam = viewpoint_stack.pop()
+    render_pkg = count_render(viewpoint_cam, gaussians, pipe, background)
+    gaussian_list, imp_list = (
+        render_pkg["gaussians_count"],
+        render_pkg["important_score"],
+    )
+
+    for iteration in range(len(viewpoint_stack)):
+        viewpoint_cam = viewpoint_stack.pop()
+        render_pkg = count_render(viewpoint_cam, gaussians, pipe, background)
+        gaussians_count, important_score = (
+            render_pkg["gaussians_count"].detach(),
+            render_pkg["important_score"].detach(),
+        )
+        gaussian_list += gaussians_count
+        imp_list += important_score
+        gc.collect()
+    return gaussian_list, imp_list
